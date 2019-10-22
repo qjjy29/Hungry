e420a02e87fefbf6e9acacabac61583f54b224b3
Jessica Moyer <moyer.247@buckeyemail.osu.edu>
2019-10-22 08:19:04 -0400

    Implemented update operation for trucks

--- app/src/main/java/com/osueat/hungry/ui/vendor/VendorAddTruckActivity.kt
+++ app/src/main/java/com/osueat/hungry/ui/vendor/VendorAddTruckActivity.kt
@@ -7,8 +7,6 @@ import android.text.TextUtils
 import android.util.Log
 import android.util.Log.d
 import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -17,7 +15,6 @@ import com.osueat.hungry.R
 import com.osueat.hungry.model.Truck
 import kotlinx.android.synthetic.main.activity_vendor_add_truck.*
 import android.nfc.Tag
-import android.widget.ListView
 import com.osueat.hungry.model.TruckDao
 import java.util.*
 import kotlin.collections.ArrayList
@@ -25,6 +22,13 @@ import kotlin.collections.HashMap
 import com.osueat.hungry.MainActivity
 import android.text.method.TextKeyListener.clear
 import com.osueat.hungry.model.TruckListAdapter
+import android.view.LayoutInflater
+import android.app.AlertDialog
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_vendor_add_truck.addressEditText
+import kotlinx.android.synthetic.main.activity_vendor_add_truck.nameEditText
+import kotlinx.android.synthetic.main.layout_update_delete_truck.*
+import kotlinx.android.synthetic.main.layout_update_delete_truck.view.*
 
 
 class VendorAddTruckActivity : AppCompatActivity() {
@@ -38,7 +42,7 @@ class VendorAddTruckActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vendor_add_truck)
 
-        // add truck to database
+        // add truck to database when save button pressed
         saveButton.setOnClickListener(View.OnClickListener {
             val name = nameEditText.text
             val address = addressEditText.text
@@ -64,6 +68,65 @@ class VendorAddTruckActivity : AppCompatActivity() {
                     Toast.makeText(this, "Please enter an address", Toast.LENGTH_LONG).show()
             }
         })
+
+        // add on click listener to list view buttons to go to truck page
+        findViewById<ListView>(R.id.truckListView).setOnItemClickListener(AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            val t = truckList.get(i)
+            createUpdateTruckWindow(t.getId(), t.getName(), t.getAddress())
+        })
+
+    }
+
+    private fun createUpdateTruckWindow(truckId: String, truckName: String, truckAddress: String) {
+        val alertDialog = AlertDialog.Builder(this@VendorAddTruckActivity)
+        val updateView = layoutInflater.inflate(R.layout.layout_update_delete_truck, null)
+        alertDialog.setView(updateView)
+        alertDialog.setTitle(truckName)
+
+        // show update/delete window
+        val alertWindow = alertDialog.create()
+        alertWindow.show()
+
+        updateView.updateTruckButton.setOnClickListener(View.OnClickListener {
+            val newName = updateView.nameEditText.text.toString()
+            val newAddress = updateView.addressEditText.text.toString()
+
+            // update both name and address of truck
+            if (!TextUtils.isEmpty(newName) && !TextUtils.isEmpty(newAddress)) {
+                updateTruck(truckId, newName, newAddress)
+                alertWindow.dismiss()
+                Toast.makeText(this, "Truck name and address updated", Toast.LENGTH_LONG).show()
+            }
+
+            else if (TextUtils.isEmpty((newName)) || TextUtils.isEmpty((newAddress))) {
+                // update only the address; name remains unchanged
+                if (TextUtils.isEmpty((newName)) && !TextUtils.isEmpty((newAddress))) {
+                    updateTruck(truckId, truckName, newAddress)
+                    Toast.makeText(this, "Truck address updated", Toast.LENGTH_LONG).show()
+                    alertWindow.dismiss()
+                }
+
+                // update only name; address remains unchanged
+                else if (!TextUtils.isEmpty((newName)) && TextUtils.isEmpty((newAddress))) {
+                    updateTruck(truckId, newName, truckAddress)
+                    Toast.makeText(this, "Truck name updated", Toast.LENGTH_LONG).show()
+                    alertWindow.dismiss()
+                }
+
+                else if (TextUtils.isEmpty(newName) && TextUtils.isEmpty(newAddress)) {
+                    Toast.makeText(this, "Please enter a new name or address", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+        //deleteButton.setOnClickListener(View.OnClickListener {
+        //})
+    }
+
+    private fun updateTruck(truckId: String, newName: String, newAddress: String) {
+        val truckRef = FirebaseDatabase.getInstance().getReference("trucks").child(truckId)
+        val newTruck = Truck(truckId, newName, newAddress)
+        truckRef.setValue(newTruck)
     }
 
     public override fun onStart() {
