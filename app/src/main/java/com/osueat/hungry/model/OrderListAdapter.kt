a4c0c9bd7468629e14a7da3a304b3c2ece4e189b
Jessica Moyer <moyer.247@buckeyemail.osu.edu>
2019-11-06 23:23:17 -0500

    Customers can view a list of all the orders they have made

---
+++ app/src/main/java/com/osueat/hungry/model/OrderListAdapter.kt
@@ -0,0 +1,31 @@
+package com.osueat.hungry.model
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.google.firebase.database.FirebaseDatabase
+import com.osueat.hungry.R
+
+class OrderListAdapter(private val context: Activity, internal var orderList: List<Order>) :
+    ArrayAdapter<Order>(context, R.layout.layout_order_list, orderList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+        val listViewItem = inflater.inflate(R.layout.layout_order_list, null, true)
+
+        val order = orderList[position]
+
+        val nameTextView = listViewItem.findViewById(R.id.nameTextView) as TextView
+        val orderDateTextView = listViewItem.findViewById(R.id.orderDateTextView) as TextView
+        val statusTextView = listViewItem.findViewById(R.id.statusTextView) as TextView
+
+        //todo: change truck id to the truck's name
+        nameTextView.text = order.truckId
+        orderDateTextView.text = order.createDate.toString()
+        statusTextView.text = order.status
+
+        return listViewItem
+    }
+}
