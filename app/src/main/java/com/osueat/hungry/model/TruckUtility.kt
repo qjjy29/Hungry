2db49692eac4ce3de83569a5fd7dc6d0c8158f2e
Jessica Moyer <moyer.247@buckeyemail.osu.edu>
2019-11-20 23:19:08 -0500

    Added some methods and unit tests for functions in TruckUtlity

--- app/src/main/java/com/osueat/hungry/model/TruckUtility.kt
+++ app/src/main/java/com/osueat/hungry/model/TruckUtility.kt
@@ -11,4 +11,32 @@ class TruckUtility {
 
         return activeTruckList
     }
+
+    fun GetInactiveTrucksFromList(truckList: ArrayList<Truck>) : ArrayList<Truck> {
+        val inactiveTruckList = ArrayList<Truck>()
+
+        for(i in 0..truckList.size - 1) {
+            if (!truckList[i].isActive)
+                inactiveTruckList.add(truckList[i])
+        }
+
+        return inactiveTruckList
+    }
+
+    fun DeactivateAllActiveTrucksInList(truckList: ArrayList<Truck>) : ArrayList<Truck> {
+        val deactivatedTrucks = ArrayList<Truck>()
+
+        for(i in 0..truckList.size - 1) {
+            if (truckList[i].isActive) {
+                val deactivatedTruck = Truck(truckList[i].id, truckList[i].name, truckList[i].address,
+                    truckList[i].foodIdList, truckList[i].vendorId, false, truckList[i].latitude, truckList[i].longitude)
+
+                deactivatedTrucks.add(deactivatedTruck)
+            } else {
+                deactivatedTrucks.add(truckList[i])
+            }
+        }
+
+        return deactivatedTrucks
+    }
 }
