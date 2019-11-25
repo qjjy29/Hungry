d73ca205f09b47c1a4b5ceeaac559ecd4d27e220
Jessica Moyer <moyer.247@buckeyemail.osu.edu>
2019-11-25 10:45:42 -0500

    Added screen rotation functionality for customer reviews

--- app/src/main/java/com/osueat/hungry/ui/customer/CustomerTruckInfoActivity.kt
+++ app/src/main/java/com/osueat/hungry/ui/customer/CustomerTruckInfoActivity.kt
@@ -2,6 +2,8 @@ package com.osueat.hungry.ui.customer
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -21,6 +23,7 @@ import com.osueat.hungry.model.*
 import kotlinx.android.synthetic.main.activity_customer_truck_info.*
 import kotlinx.android.synthetic.main.activity_customer_truck_info.view.addReviewButton
 import kotlinx.android.synthetic.main.layout_add_review.view.*
+import kotlinx.android.synthetic.main.layout_update_delete_truck.view.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -30,22 +33,63 @@ class CustomerTruckInfoActivity : AppCompatActivity() {
     private val TAG = "TruckInfoActivity"
 
     private val foodList = ArrayList<Food>()
-    private val reviewList = ArrayList<Review>()
+    private var reviewList = ArrayList<Review>()
     private val ref = FirebaseDatabase.getInstance().reference
     private val foodDao = FoodDao(ref)
     private val reviewDao = ReviewDao(ref)
 
+    // values stored if screen is rotated
+    private var addReviewWindowOpen = false
+    private var previousReviewText = ""
+    private var previousScore = ""
 
-    private fun addReviewWindow(truckName: String) {
+    private fun addReviewWindow(truckName: String, restoredValues : Bundle?) {
+        addReviewWindowOpen = true
         // add reviews
         val addReviewAlertDialog = AlertDialog.Builder(this@CustomerTruckInfoActivity)
         val addReviewView = layoutInflater.inflate(R.layout.layout_add_review, null)
         addReviewAlertDialog.setView(addReviewView)
         addReviewAlertDialog.setTitle(truckName)
 
+        addReviewAlertDialog.setOnCancelListener {
+            addReviewWindowOpen = false
+            previousReviewText = ""
+            previousScore = ""
+        }
+
         // show add review window
         val alertWindow = addReviewAlertDialog.create()
         alertWindow.show()
+
+        if (restoredValues != null) {
+            previousReviewText = restoredValues!!.getString("previousReviewText").toString()
+            previousScore = restoredValues!!.getString("previousScore").toString()
+
+            addReviewView.reviewEditText.setText(restoredValues!!.getString("previousReviewText"))
+            addReviewView.starEditText.setText(restoredValues!!.getString("previousScore"))
+        }
+
+        addReviewView.reviewEditText.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                previousReviewText = addReviewView.reviewEditText.text.toString()
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+        })
+
+        addReviewView.starEditText.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                previousScore = addReviewView.starEditText.text.toString()
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+        })
 
         addReviewView.addReviewButton.setOnClickListener {
             // parameters
@@ -62,11 +106,36 @@ class CustomerTruckInfoActivity : AppCompatActivity() {
             reviewDao.createReview(newReview)
             Toast.makeText(applicationContext, "Thank you! Your review has been posted!", Toast.LENGTH_LONG).show()
             alertWindow.dismiss()
+            addReviewWindowOpen = false
+            previousReviewText = ""
+            previousScore = ""
         }
 
         addReviewView.cancelReviewButton.setOnClickListener(View.OnClickListener {
             alertWindow.dismiss()
+            addReviewWindowOpen = false
+            previousReviewText = ""
+            previousScore = ""
         })
+    }
+
+    override fun onSaveInstanceState(bundle: Bundle) {
+        super.onSaveInstanceState(bundle)
+        bundle.putBoolean("addReviewWindowOpen", addReviewWindowOpen)
+        bundle.putParcelableArrayList("reviewList", reviewList)
+        bundle.putString("previousReviewText", previousReviewText)
+        bundle.putString("previousScore", previousScore)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState")
+
+        reviewList = savedInstanceState!!.getParcelableArrayList<Review>("reviewList")!!
+
+        if (savedInstanceState!!.getBoolean("addReviewWindowOpen")) {
+            addReviewWindow(intent.getStringExtra("truckName"), savedInstanceState)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -97,7 +166,7 @@ class CustomerTruckInfoActivity : AppCompatActivity() {
 
         val addReviewButton = findViewById<Button>(R.id.addReviewButton)
         addReviewButton.setOnClickListener {
-            addReviewWindow(intent.getStringExtra("truckName"))
+            addReviewWindow(intent.getStringExtra("truckName"), null)
         }
     }
 
